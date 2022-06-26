457252320d04a3e5bcfd65f3b379ba342173d87d
Diego Damaceno dos Santos <78373208+diego780705@users.noreply.github.com>
2022-06-26 15:15:57 -0300

    Update InstalaçãoNode-RED.sh

--- InstalaçãoNode-RED.sh
+++ InstalaçãoNode-RED.sh
@@ -4,15 +4,7 @@ sudo -i
 
 timedatectl set-timezone America/Sao_Paulo
 
-apt update -y && apt upgrade -y && apt autoremove -y && apt autoclean -y
-
-fallocate -l 3G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-swapon --show
-cp /etc/fstab /etc/fstab.bak
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+apt update -y && apt upgrade -y && apt autoremove -y 
 
 apt-get install \
 jq \
@@ -27,11 +19,29 @@ vim \
 openssh-server \
 dbus -y
 
+fallocate -l 3G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+swapon --show
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
 systemctl enable firewalld
 firewall-cmd --permanent --add-port=1-65535/tcp
 firewall-cmd --reload
 systemctl start firewalld
 
-curl -fsSL get.docker.com | sh
-sleep 10
-docker run -it -p 1880:1880 -v myNodeREDdata:/data --name mynodered nodered/node-red:latest-minimal
+echo "Include /etc/ssh/sshd_config.d/*.conf" > /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
+echo "UsePAM yes" >> /etc/ssh/sshd_config
+echo "X11Forwarding yes" >> /etc/ssh/sshd_config
+echo "PrintMotd no" >> /etc/ssh/sshd_config
+echo "AcceptEnv LANG LC_*" >> /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "Subsystem       sftp    /usr/lib/openssh/sftp-server" >> /etc/ssh/sshd_config
+service ssh restart
+
+sudo npm install -g --unsafe-perm node-red
